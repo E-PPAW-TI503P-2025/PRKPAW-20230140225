fd237e85e19700f893e6172abb78dee1f656eb4f
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-30 19:08:27 +0700

    Tugas5

--- 20230140225-node-server/controllers/reportController.js
+++ 20230140225-node-server/controllers/reportController.js
@@ -1,9 +1,41 @@
-const presensiRecords = require("../data/presensiData");
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
-};
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
 
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama, tanggal } = req.query; 
+    let options = { where: {} };
+
+   
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+ 
+    if (tanggal) {
+      const startOfDay = new Date(tanggal);
+      startOfDay.setHours(0, 0, 0, 0);
+      const endOfDay = new Date(tanggal);
+      endOfDay.setHours(23, 59, 59, 999);
+
+      options.where.createdAt = {
+        [Op.between]: [startOfDay, endOfDay],
+      };
+    }
+
+    
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString("id-ID"),
+      data: records,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Gagal mengambil laporan",
+      error: error.message,
+    });
+  }
+};
--- 20230140225-node-server/routes/presensi.js
+++ 20230140225-node-server/routes/presensi.js
@@ -5,4 +5,16 @@ const { addUserData } = require('../middleware/permissionMiddleware');
 router.use(addUserData);
 router.post('/check-in', presensiController.CheckIn);
 router.post('/check-out', presensiController.CheckOut);
+router.post("/check-in", presensiController.CheckIn);
+router.post("/check-out", presensiController.CheckOut);
+
+router.delete("/:id", presensiController.deletePresensi);
+router.post("/check-in", presensiController.CheckIn);
+router.post("/check-out", presensiController.CheckOut);
+
+router.put("/:id", presensiController.updatePresensi);
+
+
 module.exports = router;
+
+
