f91a4ab52278658ec32f0df34ebd26f6e7cc1fb4
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-12-03 23:14:11 +0700

    perubahan

--- 20230140225-node-server/server.js
+++ 20230140225-node-server/server.js
@@ -22,12 +22,14 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.send("Home Page for API");
 });
+
 const ruteBuku = require("./routes/books");
 app.use("/api/books", ruteBuku);
 app.use("/api/presensi", presensiRoutes);
 app.use("/api/reports", reportRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
